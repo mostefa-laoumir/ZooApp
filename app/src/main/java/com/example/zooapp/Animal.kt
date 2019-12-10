23ebaae274aaba8061069ea169073ace170bbc67
Laoumir Mustapha Abdelkader <21nounou21@gmail.com>
2019-12-10 21:35:32 +0100

    added new class Animal with constructor

---
+++ app/src/main/java/com/example/zooapp/Animal.kt
@@ -0,0 +1,13 @@
+package com.example.zooapp
+
+class Animal {
+    var name:String?= null
+    var des:String? = null
+    var image:Int? = null
+
+    constructor(name:String, des:String, image:Int){
+        this.name = name
+        this.des = des
+        this.image = image
+    }
+}
